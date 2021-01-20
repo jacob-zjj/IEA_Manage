3324ebad15b76c9f285dfe03414503042b6110e6
jacob-zjj <1213608068@qq.com>
2021-01-20 16:40:03 +0800

    updae

--- 项目修改/IOTEWebManagementPlatform/IOTEWMPApp/apps.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class AddressbookappConfig(AppConfig):
-    # name = "IEM"
-    name = 'IOTEWMPApp'
--- 项目修改/IOTEWebManagementPlatform/IOTEWMPApp/test.py
+++
@@ -1,5 +0,0 @@
-from . import ClientToIPv6
-ClientToIPv6 = ClientToIPv6.ClientToIPv6()
-ClientToIPv6.connect()
-res = ClientToIPv6.atPressure7()
-print(res)
